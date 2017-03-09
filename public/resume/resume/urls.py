9a1bfd1d0efa1ff081969e9987617b375423155f
Brian Alexander <brian@dadgumsalsa.com>
2017-03-09 17:14:01 -0600

    Updating some url configs in hopes to resolve static file issue in production.

--- public/resume/resume/urls.py
+++ public/resume/resume/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^(?:index|main)?/?', include('main.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
